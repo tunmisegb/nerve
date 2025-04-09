69ed67ec963b30fb4b57837f7ab04897b5a91f3a
tunmisegb <152580113+tunmisegb@users.noreply.github.com>
2025-04-09 10:54:25 +0100

    Add files via upload

---
+++ starter/otp.js
@@ -0,0 +1,9 @@
+function sendOTP() {
+    const email = document.getElementById('email');
+    const otpverify = document.getElementsByClassName('otpverify')[0];
+
+    Email.send({
+    SecureToken : " C972D7AD-F097-4895-91F4-40ABC5567812"
+   
+    })
+}
